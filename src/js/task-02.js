da5e51b21c90c90dd34aabf3c115c9ba6014126b
jcaror99 <121507887+jcaror99@users.noreply.github.com>
2023-08-28 19:19:34 -0500

    TaskTwo

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,10 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const list = document.querySelector('#ingredients');
+const liItems = document.createElement('li');
+liItems.textContent = 'Potatoes';
+liItems.classList.add('item');
+list.append(liItems);
+
+const joinIngredients = ingredients.map((element) => `<li class="item">${element}</li>`).join('');
+list.innerHTML = joinIngredients;
