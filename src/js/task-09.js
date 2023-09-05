759d06b2c68d743168280aebf4a00e944d22404f
jcaror99 <121507887+jcaror99@users.noreply.github.com>
2023-09-05 09:13:22 -0500

    taskNine

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,5 +1,17 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+	return `#${Math.floor(Math.random() * 16777215)
+		.toString(16)
+		.padStart(6, 0)}`;
 }
+
+const changeButton = document.querySelector('.change-color');
+const spanText = document.querySelector('.color');
+const bodyColor = document.querySelector('body');
+
+const changeColor = (e) => {
+	const newColor = getRandomHexColor();
+	spanText.textContent = newColor;
+	bodyColor.setAttribute('style', 'background-color:' + newColor);
+};
+
+changeButton.addEventListener('click', changeColor);
