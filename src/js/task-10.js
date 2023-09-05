e5f29e42cf6e50c81a9a930f2ae7b8d5de183bfc
jcaror99 <121507887+jcaror99@users.noreply.github.com>
2023-09-05 11:02:16 -0500

    taskTen

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,5 +1,34 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+	return `#${Math.floor(Math.random() * 16777215)
+		.toString(16)
+		.padStart(6, 0)}`;
 }
+
+const createBoxes = (amount) => {
+	const newDiv = [];
+	let dimensions = 20;
+	for (let i = 0; i < amount; i += 1) {
+		let newDimension = (dimensions += 10);
+		newDiv.push(
+			`<div id='${i}' style="width: ${newDimension}px; height: ${newDimension}px; background-color: ${getRandomHexColor()}">${newDimension}</div>`,
+		);
+	}
+	return newDiv.join('');
+};
+
+const createButton = document.querySelector('button[data-create]');
+const destroyButton = document.querySelector('button[data-destroy]');
+const divList = document.querySelector('#boxes');
+const quantityDiv = document.querySelector('input');
+
+const createDiv = (e) => {
+	const quantityDivValue = quantityDiv.value;
+	divList.innerHTML = createBoxes(quantityDivValue);
+};
+
+const destroyDiv = () => {
+	divList.innerHTML = '';
+};
+
+createButton.addEventListener('click', createDiv);
+destroyButton.addEventListener('click', destroyDiv);
