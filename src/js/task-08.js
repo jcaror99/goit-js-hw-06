8ba4c7ed4969f420d094034f8c1779db2c6ab346
jcaror99 <121507887+jcaror99@users.noreply.github.com>
2023-09-04 22:55:46 -0500

    taskEight

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,17 @@
+const loginForm = document.querySelector('form');
+const submitButton = (e) => {
+	e.preventDefault();
 
+	const email = loginForm.elements.email.value;
+	const pwd = loginForm.elements.password.value;
+	const currentEvent = e.currentTarget;
+
+	if (email.includes(' ') || pwd.includes(' ')) {
+		alert('todos los espacios deben ser rellenados');
+	} else {
+		console.log(`email: ${email} y password: ${pwd}`);
+		currentEvent.reset();
+	}
+};
+
+loginForm.addEventListener('submit', submitButton);
